578535810b4fba531c2577e9d7c9185b782a188c
sto-pau <stoccop@stanford.edu>
2023-05-23 22:53:24 -0700

    changes to simplify image output, need to be made in python folder not local folder

--- flappy_bird_gym/envs/__init__.py
+++ flappy_bird_gym/envs/__init__.py
@@ -1,3 +1,4 @@
+# MIT License
 #
 # Copyright (c) 2020 Gabriel Nogueira (Talendar)
 #
@@ -20,8 +21,47 @@
 # SOFTWARE.
 # ==============================================================================
 
-""" Exposes the environment class.
+""" Registers the gym environments and exports the `gym.make` function.
 """
 
+# Silencing pygame:
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+# Exporting envs:
 from flappy_bird_gym.envs.flappy_bird_env_rgb import FlappyBirdEnvRGB
 from flappy_bird_gym.envs.flappy_bird_env_simple import FlappyBirdEnvSimple
+
+from flappy_bird_gym.envs.flappy_bird_env_rgb import FlappyBirdEnvRGBDeepRL
+
+# Exporting original game:
+from flappy_bird_gym import original_game
+
+# Exporting gym.make:
+from gym import make
+
+# Registering environments:
+from gym.envs.registration import register
+
+register(
+    id="FlappyBird-v0",
+    entry_point="flappy_bird_gym:FlappyBirdEnvSimple",
+)
+
+register(
+    id="FlappyBird-rgb-v0",
+    entry_point="flappy_bird_gym:FlappyBirdEnvRGB",
+)
+
+register(
+    id="FlappyBird-rgb-v1",
+    entry_point="flappy_bird_gym:FlappyBirdEnvRGBDeepRL",
+)
+
+# Main names:
+__all__ = [
+    make.__name__,
+    FlappyBirdEnvRGB.__name__,
+    FlappyBirdEnvSimple.__name__,
+    FlappyBirdEnvRGBDeepRL.__name__,
+]
