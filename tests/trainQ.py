eceee696437adb27cd608b289978800903e0fae0
sto-pau <stoccop@stanford.edu>
2023-05-30 18:00:40 -0700

    written draft, need to test

--- tests/trainQ.py
+++ tests/trainQ.py
@@ -1,9 +1,14 @@
 import numpy as np
 from networkQapprox import *
 import csv
+import pandas as pd
+
+def readCSV(path):
+    #from this data we obtain the dataset and the variables
+    df = pd.read_csv(path)     
+    return df
 
 # Define the state and action spaces
-
 state_dim = 5
 action_dim = 2
 hidden_dim = 10
@@ -11,33 +16,40 @@ hidden_dim = 10
 # Initialize the QLearning agent
 agent = QLearningAgent(state_dim, action_dim, hidden_dim)
 
-train_steps = 
-# intialize state
-state = np.array([1.2, 3.4, -4]).reshape(-1,3)
+#Read in training data into a data frame
+#data should be in the same folder, input to function should be target file name and return file name
+save_path = '/home/paulalinux20/Optimal_Control_Project/flappy-bird-gym/data'
+file_name = 'data_d29t184122'    
+path = save_path + file_name + '.csv'
 
-def action_space(action,c1,c2):
-    act = [45, -45, 12][action]
-    return np.array([np.sin(act)*c1,c1+np.random.choice([-c2,c2]),  np.cos(act)+c2]).reshape(-1,3)
+df = readCSV(path) #obtain the dataset and the variables from csv file 
+#x0,x1,y0,y1,v,a,r
 
-# train
-for i in range(train_steps):
-    # Choose an action using the epsilon-greedy policy
-    action = agent.get_training_action(state)
-    # Take the action and observe the reward and new state
-    reward = np.sum(state**2 - state/2) + np.random.rand()
-    print(state[0],action)
-    next_state = action_space(action,np.random.rand(),np.random.rand())
-    # Update the Q-value for the current state-action pair
-    agent.update_Q(state, action, reward, next_state)
-    # Update the current state and check if the episode is done
-    state = next_state
-    agent.epsilon = agent.epsilon * 0.999
-    
+#get initial state, action, reward
+initial_data= df.iloc[0]
+state = initial_data[:,5]
+action = initial_data[-2]
+reward = initial_data[-1]
+
+#skip first row
+first_row = True
+
+# train through all the rows in saved data one time
+for row in df.itertuples(): #options are iterrows(), values()
+    if first_row:
+        pass
+    else:
+        #index for all values
+        next_state = row[:,5]
+        agent.update_Q(state, action, reward, next_state)
+        state = row[:,5]
+        action = row[-2]
+        reward = row[-1]
+   
 # to save    
 agent.save_agent('agent.pkl')
 # load
-q_agent = QLearningAgent.load_agent('agent.pkl')
-     
+q_agent = QLearningAgent.load_agent('agent.pkl')     
 
 #
 # # Test the agent by following the greedy policy
